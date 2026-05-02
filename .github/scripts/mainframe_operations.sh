e9f5ed0a04e294d21ba51bce28ee2718af23e72b
François Luc Denhez-Teuton <80438929+FrancoisLucDT@users.noreply.github.com>
2026-05-02 22:47:33 +0000

    Update mainframe_operations.sh to use $ZOWE_USERNAME

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -11,7 +11,7 @@ export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
 java -version
 
 # Set ZOWE_USERNAME
-ZOWE_USERNAME="${{ secrets.ZOWE_USERNAME }}"  # Replace with your actual username
+ZOWE_USERNAME="$ZOWE_USERNAME"  # Replace with your actual username
 
 # Change to the cobolcheck directory
 cd cobolcheck
